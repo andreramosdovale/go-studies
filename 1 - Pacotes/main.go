824e80f2b013e05fbe16fca14a31c53f7e1c8072
André Vale <andre_ramosdovale@outlook.com>
2025-01-01 17:48:16 -0300

    feat: pacotes externos

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	auxiliar.Escrever()
 	auxiliar.Escrever2()
 	fmt.Println("escrevendo do arquivo main")
+	err := checkmail.ValidateFormat("andre.vale@a.com")
+	fmt.Println(err)
 }
