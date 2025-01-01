7ec85c38685ab3512c7b332e2242a8a6f7d8231d
André Vale <andre_ramosdovale@outlook.com>
2025-01-01 17:24:37 -0300

    feat: estudo sobre pacotes

---
+++ 1 - Pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,11 @@
+package auxiliar
+
+import "fmt"
+
+func escrever2() {
+	fmt.Println("dentro do escrever 2")
+}
+
+func Escrever2() {
+	escrever2()
+}
---
+++ 1 - Pacotes/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	"modulo/auxiliar"
+)
+
+func main() {
+	auxiliar.Escrever()
+	auxiliar.Escrever2()
+	fmt.Println("escrevendo do arquivo main")
+}
